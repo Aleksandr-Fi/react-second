9beb16d2464b99ccedf7fb31a597c2c0add955de
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-14 21:14:35 +0300

    task to class

--- src/components/task.js
+++ src/components/task.js
@@ -1,18 +1,25 @@
-import NewTaskForm from "./new-task-form";
+import { Component } from "react"
 
-const Task = ( {description, created, field} ) => {
+
+class Task extends Component {
+
+
+  render() {
+    
+    const {description, created} = this.props
 
     return (
-        <div>
-          <input className="toggle" type="checkbox" />
-          <label>
-            <span className="description">{description}</span>
-            <span className="created">{created}</span>
-          </label>
-          <button className="icon icon-edit"></button>
-          <button className="icon icon-destroy"></button>
-        </div>
-    )
+      <div>
+        <input className="toggle" type="checkbox" />
+        <label>
+          <span className="description">{description}</span>
+          <span className="created">{created}</span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+  )
+  }
 }
 
 export default Task
