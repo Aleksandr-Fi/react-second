f7434147513d6f53df79237dce29724f1aa0a5d3
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-20 18:22:22 +0300

    newTaskForm to class

--- src/components/new-task-form.js
+++ src/components/new-task-form.js
@@ -1,8 +1,42 @@
+import { Component } from "react"
 
 
-const NewTaskForm = ( {value} ) => {
+class NewTaskForm extends Component {
 
-    return <input type="text" className="edit" defaultValue={value} />
+    state = {
+    text: ''
+    }
+
+    onChengeText = (e) => {
+        this.setState({
+        text: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        // e.preventDefault()
+        // this.props.addTask(this.state.text)
+        console.log(this.state.text);
+        this.setState({
+        text: ''
+        })
+    }
+
+    render() {
+        const {value} = this.props
+
+        return (
+            <form className=""
+                    onSubmit={this.onSubmit}>
+                <input type="text" 
+                    className="edit" 
+                    defaultValue={value}
+                    onChange={this.onChengeText} />
+            </form>
+        
+        )
+    }
+    
 }
 
 export default NewTaskForm
