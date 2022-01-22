1f066de0f1757fc972c14598e49d06509d7ec797
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-22 19:56:30 +0300

    task is not a class

--- src/components/task.js
+++ src/components/task.js
@@ -1,40 +1,37 @@
-import { Component } from "react"
 import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
 import NewTaskForm from './new-task-form'
 
-class Task extends Component {
+const Task = (props) => {
+  
+  const {completed, editing, text, created,
+      onDestroy, onEditing, onCompleted, onChangeForm} = props
 
-  render() {
-    const {completed, editing, text, created,
-        onDestroy, onEditing, onCompleted, onChangeForm} = this.props
-
-    let classNames = ''
-    if (completed) {
-      classNames += ' completed'
-    }
-    if (editing) {
-      classNames += ' editing'
-    }
-
-    return (
-      <li className={classNames}>
-        <div className="view">
-          <input  className="toggle" type="checkbox"
-                  onClick={onCompleted} />
-          <label>
-            <span className="description">{text}</span>
-            <span className="created">{`created ${formatDistanceToNowStrict(created)} ago`}</span>
-          </label>
-          <button className="icon icon-edit"
-                  onClick={onEditing}></button>
-          <button className="icon icon-destroy"
-                  onClick={onDestroy}></button>
-        </div>
-        <NewTaskForm value={text}
-                      onChangeForm={(text) => onChangeForm(text)} />
-      </li>
-    )
+  let classNames = ''
+  if (completed) {
+    classNames += ' completed'
   }
+  if (editing) {
+    classNames += ' editing'
+  }
+
+  return (
+    <li className={classNames}>
+      <div className="view">
+        <input  className="toggle" type="checkbox"
+                onClick={onCompleted} />
+        <label>
+          <span className="description">{text}</span>
+          <span className="created">{`created ${formatDistanceToNowStrict(created)} ago`}</span>
+        </label>
+        <button className="icon icon-edit"
+                onClick={onEditing}></button>
+        <button className="icon icon-destroy"
+                onClick={onDestroy}></button>
+      </div>
+      <NewTaskForm value={text}
+                    onChangeForm={(text) => onChangeForm(text)} />
+    </li>
+  )
 }
 
 export default Task
