2dea09c8a0276f7eec37516c6ce4c70abee57d82
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-02-12 11:11:43 +0300

    fixed TaskList
    fix "мапать jsx нужно в самой компоненте нежели создавать отдельную функцию где будешь мапать и возвращать переменную"

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -21,22 +21,25 @@ const TaskList = ({ todos, filters, onDestroy, onEditing, onCompleted, onChangeT
   if (filter === 'Completed') {
     taskArr = taskArr.filter((el) => el.completed)
   }
-  const elements = taskArr.map((item) => {
-    const { id, ...itemProps } = item
 
-    return (
-      <Task
-        key={id}
-        {...itemProps}
-        onDestroy={() => onDestroy(id)}
-        onEditing={() => onEditing(id)}
-        onCompleted={() => onCompleted(id)}
-        onChangeForm={(text) => onChangeTask(text, id)}
-      />
-    )
-  })
+  return (
+    <ul className="todo-list">
+      {taskArr.map((item) => {
+        const { id, ...taskData } = item
 
-  return <ul className="todo-list">{elements}</ul>
+        return (
+          <Task
+            key={id}
+            {...taskData}
+            onDestroy={() => onDestroy(id)}
+            onEditing={() => onEditing(id)}
+            onCompleted={() => onCompleted(id)}
+            onChangeForm={(text) => onChangeTask(text, id)}
+          />
+        )
+      })}
+    </ul>
+  )
 }
 
 export default TaskList
