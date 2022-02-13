8964a1bed7cfe3d478c0645705c0afad767d65e8
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-02-13 15:25:46 +0300

    Task to class component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,11 @@
 import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict'
 import propTypes from 'prop-types'
+import { Component } from 'react'
 
 import NewTaskForm from '../NewTaskForm/NewTaskForm'
 
-const Task = (props) => {
-  Task.propTypes = {
+export default class Task extends Component {
+  propTypes = {
     completed: propTypes.bool.isRequired,
     editing: propTypes.bool.isRequired,
     text: propTypes.string.isRequired,
@@ -15,39 +16,41 @@ const Task = (props) => {
     onChangeForm: propTypes.func.isRequired,
   }
 
-  const { completed, editing, text, created, checked, onDestroy, onEditing, onCompleted, onChangeForm } = props
-
-  let classNames = ''
-  if (completed) {
-    classNames += 'completed'
+  getClassName() {
+    let classNames = ''
+    if (this.props.completed) {
+      classNames += 'completed'
+    }
+    if (this.props.editing) {
+      classNames += ' editing'
+    }
+    return classNames
   }
-  if (editing) {
-    classNames += ' editing'
-  }
 
-  Task.defaultProps = {
+  defaultProps = {
     onDestroy: () => {},
     onEditing: () => {},
     onCompleted: () => {},
     onChangeForm: () => {},
   }
 
-  return (
-    <li className={classNames}>
-      <div className="view">
-        <label className="input-label label-toggle">
-          <input className="toggle" type="checkbox" onChange={onCompleted} defaultChecked={checked} />
-        </label>
-        <label>
-          <span className="description">{text}</span>
-          <span className="created">{`created ${formatDistanceToNowStrict(created)} ago`}</span>
-        </label>
-        <button className="icon icon-edit" title="edit" onClick={onEditing}></button>
-        <button className="icon icon-destroy" title="destroy" onClick={onDestroy}></button>
-      </div>
-      <NewTaskForm value={text} onChangeForm={(text) => onChangeForm(text)} />
-    </li>
-  )
+  render() {
+    const { text, created, checked, onDestroy, onEditing, onCompleted, onChangeForm } = this.props
+    return (
+      <li className={this.getClassName()}>
+        <div className="view">
+          <label className="input-label label-toggle">
+            <input className="toggle" type="checkbox" onChange={onCompleted} defaultChecked={checked} />
+          </label>
+          <label>
+            <span className="description">{text}</span>
+            <span className="created">{`created ${formatDistanceToNowStrict(created)} ago`}</span>
+          </label>
+          <button className="icon icon-edit" title="edit" onClick={onEditing}></button>
+          <button className="icon icon-destroy" title="destroy" onClick={onDestroy}></button>
+        </div>
+        <NewTaskForm value={text} onChangeForm={(text) => onChangeForm(text)} />
+      </li>
+    )
+  }
 }
-
-export default Task
